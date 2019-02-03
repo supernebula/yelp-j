daf30087ebf5dc1d1bde559ec2d11be1e247cb3e
supernebula <supernebula@126.com>
2019-02-03 18:44:12 +0800

    none

---
+++ yelp/yelp-web-manage/src/main/java/com/yelp/web/manage/YelpWebManageApplication.java
@@ -0,0 +1,14 @@
+package com.yelp.web.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpWebManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YelpWebManageApplication.class, args);
+    }
+
+}
+
