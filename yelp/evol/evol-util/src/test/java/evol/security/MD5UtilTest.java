3dc2824385d736351b712e4d1f64d0a92dbedce0
supernebula <supernebula@126.com>
2019-02-27 18:41:29 +0800

    Dozer test

--- yelp/evol/evol-util/src/test/java/evol/security/MD5UtilTest.java
+++ yelp/evol/evol-util/src/test/java/evol/security/MD5UtilTest.java
@@ -1,12 +1,11 @@
 package evol.security;
 
 import org.junit.Assert;
-
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class MD5UtilTest {
 
-    @org.junit.Test
+    @Test
     public void MD5Test() {
         String sign = MD5Util.MD5("1234567890ABCDEFG_0123456789");
         System.out.println("sign:");
@@ -14,7 +13,7 @@ public class MD5UtilTest {
         Assert.assertEquals(sign, "f6a66a0908db4f9650bc873fc4aebe62");
     }
 
-    @org.junit.Test
+    @Test
     public void MD5BySaltTest() {
         String sign = MD5Util.MD5("1234567890ABCDEFG_0123456789", "SALT0123");
         System.out.println("sign:");
---
+++ yelp/yelp-web-manage/src/test/java/com/basic/DozerMapperTest.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class DozerMapperTest {
+
+    @Test
+    public void Test1(){}
+}
