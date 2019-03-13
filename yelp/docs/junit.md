26634a46fac36e9019f322022708f958fbce5412
supernebula <supernebula@126.com>
2019-03-13 13:19:50 +0800

    redis test String key-value ok

--- yelp/docs/junit.md
+++ yelp/docs/junit.md
@@ -20,3 +20,7 @@ https://blog.csdn.net/goligory/article/details/81737523
 ## Junit、Assert、内省、Properties类与配置文件的使用
 
 https://blog.csdn.net/wust__wangfan/article/details/53164533
+
+## SpringBoot Junit
+
+[Spring-boot junit类读取config配置文件的注解](https://blog.csdn.net/qq_32447321/article/details/74183357)
--- yelp/yelp-cache/src/test/java/com/yelp/cache/RedisTest.java
+++ yelp/yelp-cache/src/test/java/com/yelp/cache/RedisTest.java
@@ -4,12 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootApplication
 @RunWith(SpringJUnit4ClassRunner.class)//使用junit4进行测试
-@ContextConfiguration({"classpath*:application.test.properties"}) //加载配置文件
+//@ContextConfiguration({"classpath*:application.test.properties"}) //加载配置文件,(1.4.0过期)
 public class RedisTest {
 
     @Autowired
